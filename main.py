e42291e081b29d2e69802007d9bc1aada38d3876
RobertXu09 <36168817+robxu09@users.noreply.github.com>
2024-03-12 22:40:54 -0400

    can print all champion details from scraper

--- main.py
+++ main.py
@@ -5,17 +5,39 @@ from champions.champion_data import create_champions
 from items.item_data import create_items
 from scenarios.one_vs_one import OneVsOne
 
-from scraper.scraper import get_html_content, extract_champion_data
+from scraper.scraper import get_html_content, extract_champion_urls, extract_champion_data
 
 
 
 def main():
 
     #test scraper
-    url = 'https://lolchess.gg/champions/set10/'
+    url = 'https://lolchess.gg/champions/set11/'
     html_content = get_html_content(url)
     if html_content:
-        champions_data = extract_champion_data(html_content)
+        champions_data = extract_champion_urls(html_content)
+    # for c in champions_data:
+    #     print(f"{c[1]}: {c[0]}")
+
+    for c in champions_data:
+        url = 'https://lolchess.gg' + c[0]
+        print(f'{c[1]}')
+        html_content = get_html_content(url)
+        if html_content:
+            champion_data = extract_champion_data(html_content)
+        print(f"cost: {champion_data['cost']}")
+        print(f"traits: {champion_data['traits']}")
+        print(f"health: {champion_data['health']}")
+        print(f"attack damage: {champion_data['attack damage']}")
+        print(f"ability power: {champion_data['ability power']}")
+        print(f"attack speed: {champion_data['attack speed']}")
+        print(f"armor: {champion_data['armor']}")
+        print(f"magic resist: {champion_data['magic resist']}")
+        print(f"attack range: {champion_data['attack range']}")
+        print(f"starting mana: {champion_data['starting mana']}")
+        print(f"mana to cast: {champion_data['mana to cast']}")
+        print(f"dscription: \n{champion_data['description']}")
+        print()
 
     # Get a dictionary of Champion objects
     all_champions = create_champions(10, 'test')
@@ -63,7 +85,7 @@ def main():
 
     # Create and run the one versus one scenario
     one_vs_one_scenario = OneVsOne()
-    one_vs_one_scenario.run(champion1, champion2, 50)
+    # one_vs_one_scenario.run(champion1, champion2, 50)
 
 
     # champion1.print_champion_items()
--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,5 +1,7 @@
 # scraper_module/scraper.py
 import requests
+import re
+import sys
 from bs4 import BeautifulSoup
 
 # https://lolchess.gg/champions/set10/
@@ -13,7 +15,7 @@ def get_html_content(url):
     else:
         print(f"Failed to retrieve the page. Status code: {response.status_code}")
 
-def extract_champion_data(html_content):
+def extract_champion_urls(html_content):
     soup = BeautifulSoup(html_content, 'html.parser')
 
     # Assuming the HTML structure contains information about champions
@@ -21,14 +23,118 @@ def extract_champion_data(html_content):
     champion_elements = soup.select('.css-xb1di8.e18qe9jt1 > *')
 
     champions_data = []
+    c_data = ["",""]
     for champion_element in champion_elements:
 
         if champion_element:
             href_value = champion_element.get('href')
             if href_value:
-                print(href_value)
+                # print(href_value)
+                c_data[0] = href_value
 
         if champion_element.name == 'span':
-            print(champion_element.text)
+            # print(champion_element.text)
+            c_data[1] = champion_element.text
+            champions_data.append(c_data)
+            c_data = ["",""]
+    
 
     return champions_data
+
+def extract_champion_data(html_content):
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    # Assuming the HTML structure contains information about champions
+    # Adjust these selectors based on the actual structure of the webpage
+    champion_data = {}
+
+    #scrape for cost and traits
+    champion_elements = soup.select('div.css-14ved7i.e1eohesh3')
+    if champion_elements:
+        cost_html = champion_elements[0]
+        traits_html = champion_elements[1:]
+
+        if cost_html:
+            # print(f"cost: {cost_html.text}")
+            champion_data["cost"] = cost_html.text
+        if traits_html:
+            traits = []
+            for trait_html in traits_html:
+                traits.append(trait_html.text)
+                # print(f"trait: {trait.text}")
+            champion_data["traits"] = traits
+
+#__next > div > div.css-1x48m3k.eetc6ox0 > div.content > div > section > div.css-17jx6qn.e1oq5z0y0 > div.css-j7qwjs.ed8e03y1 > article.css-0.ed8e03y0 > ul > li:nth-child(1) > div > p.css-1k9g2gq.e1ox7ks01
+    champion_elements = soup.select('p.css-1k9g2gq.e1ox7ks01')
+    if champion_elements:
+        champion_data['health'] = champion_elements[0].text
+        champion_data['attack damage'] = champion_elements[1].text
+
+
+#__next > div > div.css-1x48m3k.eetc6ox0 > div.content > div > section > div.css-17jx6qn.e1oq5z0y0 > div.css-j7qwjs.ed8e03y1 > article.css-0.ed8e03y0 > ul > li:nth-child(5) > div > p.css-fd4500.e1ox7ks02
+    champion_data['ability power'] = 100
+
+    champion_elements = soup.select('p.css-fd4500.e1ox7ks02')
+    if champion_elements:
+        champion_data['attack speed'] = champion_elements[3].text
+        champion_data['armor'] = champion_elements[4].text
+        champion_data['magic resist'] = champion_elements[5].text
+
+
+#__next > div > div.css-1x48m3k.eetc6ox0 > div.content > div > section > div.css-17jx6qn.e1oq5z0y0 > div.css-j7qwjs.ed8e03y1 > article.css-0.ed8e03y0 > ul > li:nth-child(4) > div
+        #__next > div > div.css-1x48m3k.eetc6ox0 > div.content > div > section > div.css-17jx6qn.e1oq5z0y0 > div.css-j7qwjs.ed8e03y1 > article.css-0.ed8e03y0 > ul > li:nth-child(4) > div > img
+    champion_elements = soup.select('li.css-125gcuq.e1ox7ks04 > * > img')
+    if champion_elements:
+        
+        img = champion_elements[0].get('src')
+
+        if img == "//cdn.dak.gg/tft/images2/tft/attack-distance/ico_attack_distance301.png":
+
+            champion_data['attack range'] = "1"
+        
+        elif img == "//cdn.dak.gg/tft/images2/tft/attack-distance/ico_attack_distance302.png":
+
+            champion_data['attack range'] = "2"
+        
+        else:
+
+            champion_data['attack range'] = "3+"
+
+#__next > div > div.css-1x48m3k.eetc6ox0 > div.content > div > section > div.css-17jx6qn.e1oq5z0y0 > div.css-j7qwjs.ed8e03y1 > article.css-0.ed8e03y0 > figure > figcaption > div.css-1jo0lz.emxgp8x1 > div
+    champion_elements = soup.select('div.css-xi7lq2.emxgp8x2 > *')
+    for champion_element in champion_elements:
+
+        if champion_element.name == 'span':
+            # print(champion_element.text)
+            m = champion_element.text
+            # print(f"mana: {m}")
+            pattern = r'\d+'
+
+            # Use re.findall to find all matches of the pattern in the input string
+            matches = re.findall(pattern, m)
+
+            if matches:
+                # Convert the matches to integers
+                numbers = [int(match) for match in matches]
+
+                champion_data['starting mana'] = numbers[0]
+                champion_data['mana to cast'] = numbers[1]
+            else:
+                champion_data['starting mana'] = 0
+                champion_data['mana to cast'] = sys.maxsize
+
+
+    champion_elements = soup.select('figcaption.css-x2fcwe.emxgp8x3 > *')
+    champion_data['description'] = ""
+
+    for champion_element in champion_elements:
+        # if champion_element.name == "span" or champion_element.name == "strong" or champion_element.name == "p":
+        # print(f"{champion_element.text}")
+        text_line = champion_element.get_text(separator=' ')
+        if not text_line.lower().startswith(".css-"):
+            champion_data['description'] += text_line
+            champion_data['description'] += "\n"
+            # print(f"{champion_element.get_text(separator=' ')}")
+        
+
+    return champion_data
