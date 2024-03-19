81dd8b98bd473c29726ef5ebd150f78d2a575224
RobertXu09 <36168817+robxu09@users.noreply.github.com>
2024-03-19 16:58:18 -0400

    able to run hardcoded simulation

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 # main.py
 import copy
 
-from champions.champion_data import create_champions
-from items.item_data import create_items
-from scenarios.one_vs_one import OneVsOne
-
-from simulation_system.simulator import scrape_champions_to_csv, scrape_items_to_csv, scrape_champions_and_items_to_csv
+from simulation_system.simulator import scrape_champions_to_csv, scrape_items_to_csv, scrape_champions_and_items_to_csv, run_1v1_with_two_champions
 
 
 def main():
@@ -31,47 +27,29 @@ def main():
         if user_input.lower() == 'scrapei':
             process_scrape_items_to_csv()
 
-    # main loop:
-        # 1. can import champion data to csv
-        # 2. can import item data to csv
-
-        # 3. run a simulation
-            # create all champions objects. create all items objects.
-            # ask user to select from different types of simulation options available.
-                # options include: 2 units fight. after being given items. (only option currently available)
-                    # run simulation and print (export) results
+        if user_input.lower() == 'runsim':
+            process_run_simulator()
     
-        # 4. quit
-    # end loop
-    
-
-    # # Get a dictionary of Champion objects
-    # all_champions = create_champions(10, 'test')
-
-    # Tahm_Kench = all_champions.get("Tahm Kench_1")
-    # Vi = all_champions.get("Vi_1")
-    
-    # Annie = all_champions.get("Annie_2")
-
-    # champion1 = Annie
-    # champion2 = Vi
-
-    # # Create items
-    # all_items = create_items(10, 'test')
-
-    # bt = all_items.get("Bloodthirster")
-
-    # # champion1.add_items(bt)
-    # champion2.add_items(bt)
-
-    # # Create and run the one versus one scenario
-    # one_vs_one_scenario = OneVsOne()
-    # one_vs_one_scenario.run(champion1, champion2, 50)
             
 def process_help():
     print("scrapeci: scrape champions and items data to csv")
     print("scrapec: scrape champions data to csv")
     print("scrapei: scrape items data to csv")
+    print("runsim: run simulation")
+
+def process_run_simulator():
+    user_input = input("Enter two text values separated by a space. [Set, Patch]: ")
+    # Split the input into two separate values
+    values = user_input.split()
+
+    # Check if exactly two values were provided
+    if len(values) != 2:
+        print("Please enter exactly two text values separated by a space.")
+    else:
+        value1, value2 = values
+        print(f"Scraping champions and items for Set {value1}, Patch {value2}.")
+        run_1v1_with_two_champions(value1, value2)
+
 
 def process_scrape_champions_and_items_to_csv():
     user_input = input("Enter two text values separated by a space. [Set, Patch]: ")
--- simulation_system/simulator.py
+++ simulation_system/simulator.py
@@ -1,11 +1,38 @@
 from scraper.scraper import get_html_content, extract_champion_urls, extract_champion_data, extract_items_data, export_items_to_csv, export_champion_details_to_csv
 from helper_functions import print_formatted_dict
+from champions.champion_data import create_champions
+from items.item_data import create_items
 
-def run_1v1_with_two_champions():
+from scenarios.one_vs_one import OneVsOne
+
+
+def run_1v1_with_two_champions(set, patch):
 
     # ask for input to create a champion. give error if none found
     # ask for input to add items. give available item options. keep looping and asking for if want another item
     # run onevsone with the two champions
+
+    # Get a dictionary of Champion objects
+    all_champions = create_champions(set, patch)
+
+    Vi = all_champions.get("Vi_1")
+    
+    Annie = all_champions.get("Annie_2")
+
+    champion1 = Annie
+    champion2 = Vi
+
+    # Create items
+    all_items = create_items(set, patch)
+
+    bt = all_items.get("Bloodthirster")
+
+    # champion1.add_items(bt)
+    champion2.add_items(bt)
+
+    # Create and run the one versus one scenario
+    one_vs_one_scenario = OneVsOne()
+    one_vs_one_scenario.run(champion1, champion2, 50)
 
     return
 
